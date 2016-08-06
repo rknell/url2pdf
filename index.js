a43faf0faa0699b99eacad5817810f74cb149d1f
Felix Furtmayr <ff@rapidfacture.com>
2016-08-06 12:56:14 +0200

    cleanup; option "loadTimeout"

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-"use strict";
+/*jslint node: true */
+'use strict';
+
 
 var http = require("http");
 var phantom = require("phantom");
@@ -8,130 +10,118 @@ var path = require('path');
 var findRemove = require('find-remove');
 var _ = require('lodash');
 
+
 var _opts = {
-  paperSize: {format: "A4", orientation: 'portrait', margin: '1cm'},
-  saveDir: path.join(__dirname, "pdfTemp"),
-  idLength: 30,
-  possibleIdChars: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+    paperSize: {
+        format: "A4",
+        orientation: 'portrait',
+        margin: '1cm'
+    },
+    saveDir: path.join(__dirname, "pdfTemp"),
+    idLength: 30,
+    possibleIdChars: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789",
+    loadTimeout: 800
 };
 
-function mergeOpts(newOpts) {
-  if(newOpts){
-    return _.merge({}, _opts, newOpts);
-  } else {
-    return _opts;
-  }
-
-}
-
-function renderpdf(url, opts) {
-  var deferred = q.defer();
-  var page, fileName, ph, fullPath;
-
-  opts = mergeOpts(opts);
-  phantom.create(['--ignore-ssl-errors=yes'])
-    .then(function (_ph) {
-      ph = _ph;
-      return ph.createPage()
-    })
-    .then(function (_page) {
-      page = _page;
-      return page.property("paperSize", opts.paperSize);
-    })
-    .then(function () {
-      return page.open(url)
-    })
-    .then(function (status) {
-      fileName = makeid(opts.idLength) + ".pdf";
-      fullPath = opts.saveDir + "/" + fileName;
-      return page.render(fullPath);
-    })
-    .then(function () {
-      deferred.resolve(fullPath);
-      ph.exit();
-    })
-    .catch(function (err) {
-      deferred.reject(err);
-      ph.exit();
-    });
-
-  return deferred.promise;
-}
-
-function renderFromHTML(htmlString, opts) {
-  var deferred = q.defer();
-  var page, fileName, ph, fullPath;
-
-  opts = mergeOpts(opts);
-  phantom.create(['--ignore-ssl-errors=yes'])
-    .then(function (_ph) {
-      ph = _ph;
-      return ph.createPage()
-    })
-    .then(function (_page) {
-      page = _page;
-      return q.all([
-        page.property("paperSize", opts.paperSize),
-        page.property("content", htmlString)
-      ])
-    })
-    .then(function (status) {
-      fileName = makeid(opts.idLength) + ".pdf";
-      fullPath = opts.saveDir + "/" + fileName;
-
-      return qTimeout(2000);
-    })
-    .then(function () {
-      return page.render(fullPath);
-    })
-    .then(function () {
-      deferred.resolve(fullPath);
-    })
-    .catch(function (err) {
-      deferred.reject(err);
-    })
-    .then(function () {
-      ph.exit();
-    });
-
-  return deferred.promise;
-}
-
-function qTimeout(duration){
-  var deferred = q.defer();
-
-  setTimeout(deferred.resolve, duration);
-
-  return deferred.promise;
-}
-
-function closure(fn) {
-  var _arguments = arguments;
-  delete arguments[0];
-  return function () {
-    fn.apply(_arguments);
-  }
-}
-
-function cleanup(ageInSeconds) {
-  return findRemove(_opts.saveDir, {age: {seconds: ageInSeconds}});
-}
-
-function makeid(strLength) {
-  if (!strLength) strLength = 30;
-  var text = "";
-  var possible = _opts.possibleIdChars;
-
-  for (var i = 0; i < strLength; i++) {
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
-  }
-
-  return text;
-}
 
 module.exports = {
-  renderPdf: renderpdf,
-  cleanup: cleanup,
-  opts: _opts,
-  renderFromHTML: renderFromHTML
+    opts: _opts,
+    renderPdf: function renderpdf(url, opts) {
+        return render(url, opts, "url");
+    },
+    renderFromHTML: function renderFromHTML(htmlString, opts) {
+        return render(htmlString, opts, "htmlContent");
+    },
+    cleanup: function cleanup(ageInSeconds) {
+        return findRemove(_opts.saveDir, {
+            age: {
+                seconds: ageInSeconds
+            }
+        });
+    }
 };
+
+
+
+function render(string, opts, renderType) {
+    var deferred = q.defer();
+    var page, fileName, ph, fullPath;
+
+    opts = mergeOpts(opts);
+    phantom.create(['--ignore-ssl-errors=yes'])
+        .then(function(_ph) {
+            ph = _ph;
+            return ph.createPage();
+        })
+        .then(function(_page) {
+            page = _page;
+            return page.property("paperSize", opts.paperSize);
+        })
+        .then(function() {
+            if (renderType == "url") {
+                return page.open(string); // string = url path
+            } else if (renderType == "htmlContent") {
+                return page.property("content", string); // string = html doc as string
+            } else {
+                console.log("wrong render type: " + renderType);
+                ph.exit();
+            }
+        })
+        .then(function(status) {
+            fileName = makeid(opts.idLength) + ".pdf";
+            fullPath = opts.saveDir + "/" + fileName;
+            return promiseWithTimeout(opts.loadTimeout);
+        })
+        .then(function() {
+            return page.render(fullPath);
+        })
+        .then(function() {
+            deferred.resolve(fullPath);
+        })
+        .catch(function(err) {
+            deferred.reject(err);
+        })
+        .then(function() {
+            ph.exit();
+        });
+
+    return deferred.promise;
+}
+
+
+
+function mergeOpts(newOpts) {
+    if (newOpts) {
+        return _.merge({}, _opts, newOpts);
+    } else {
+        return _opts;
+    }
+}
+
+
+function promiseWithTimeout(duration) {
+    var deferred = q.defer();
+    setTimeout(deferred.resolve, duration);
+    return deferred.promise;
+}
+
+
+function makeid(strLength) {
+    if (!strLength) strLength = 30;
+    var text = "";
+    var possible = _opts.possibleIdChars;
+
+    for (var i = 0; i < strLength; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+}
+
+
+function closure(fn) {
+    var _arguments = arguments;
+    delete arguments[0];
+    return function() {
+        fn.apply(_arguments);
+    };
+}
